b6cb9a9e57ca3a5e3064c6747e61eb3c1b2e84da
McFadden-S <shaefromhome@shaw.ca>
2018-12-01 15:02:50 -0600

    V1 Bubble sort and selection sort algorithms started

--- src/Sorter.java
+++ src/Sorter.java
@@ -26,6 +26,57 @@
  	
  	// ********** accessors **********
  	
+        /*****************************************************
+        // Purpose: sorts the list in ascending order
+        // Interface: IN: array --> list
+        //                int --> length
+        // Returns: None??
+        // *****************************************************/  
+        public void bubbleSort(int[] list, int length){
+            int x = 0; //variable for data swap
+            int y = 0; //variable for data swap
+            
+            while(length>1){
+                for(int i = 0; i < length; i++){
+                    if(list[i]>list[i+1]){
+                        x = list[i];
+                        y = list[i+1];
+                        
+                        list[i] = y;
+                        list[i+1] = x;
+                    }//end of if
+                }//end of for
+                length--;
+            }//end of while
+        }//end of bubbleSort
+        
+        /*****************************************************
+        // Purpose: sorts the list in ascending order
+        // Interface: IN: array --> list
+        //                int --> length
+        // Returns: None??
+        // *****************************************************/  
+        public void selectionSortSort(int[] list, int length){
+            int ind = 0; //variable for data swap
+            int x = 0; //variable for data swap
+            int y = 0; //variable for data swap
+            
+            while(length>0){
+                for(int i = 0; i < length; i++){
+                    if(list[i]>list[i+1]){
+                        ind = i;
+                    }//end of if
+                }//end of for
+                length--;
+                
+                x = list[ind];
+                y = list[length];
+
+                list[ind] = y;
+                list[length] = x;        
+            }//end of while
+        }//end of bubbleSort
+     
  	// ********** mutators **********
  
  }  // end class
